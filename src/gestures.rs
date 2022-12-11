80b74b0bdce5b14b7d099347d88e47ed95ce4f1f
Riley Martin <106552431+riley-martin@users.noreply.github.com>
2022-12-11 12:50:44 -0500

    Improve error handling

--- src/gestures.rs
+++ src/gestures.rs
@@ -2,10 +2,10 @@ use std::{
     fs::{File, OpenOptions},
     os::unix::prelude::{AsRawFd, FromRawFd, IntoRawFd, OpenOptionsExt, RawFd},
     path::Path,
-    process::Command,
     rc::Rc,
 };
 
+use anyhow::Result;
 use input::{
     event::{
         gesture::{
@@ -21,8 +21,9 @@ use nix::poll::{poll, PollFd, PollFlags};
 use serde::{Deserialize, Serialize};
 
 use crate::config::Config;
+use crate::utils::exec_command_from_string;
 
-// Tiny little macro to keep from having to write if statements everywhere
+/// Tiny little macro to keep from having to write if statements everywhere
 #[macro_export]
 macro_rules! if_debug {
     ($d:expr, $($item:expr),*) => {
@@ -55,7 +56,6 @@ pub enum Direction {
 impl Direction {
     // This code is sort of a mess
     pub fn dir(x: f64, y: f64) -> Direction {
-        // ratio at which a gesture is called diagonal
         if x.abs() == 0.0 && y.abs() == 0.0 {
             return Direction::C;
         }
@@ -84,7 +84,7 @@ impl Direction {
         } else {
             // Don't ask me why, but for libinput the coordinates increase downward. This does
             // hold out the same as screen coordinates, but it starts in the center instead of
-            // the upper left
+            // the upper left. I have also noticed game controllers work the same way.
             let sd = if y < 0.0 { Direction::N } else { Direction::S };
             if x.abs() / y.abs() > oblique_ratio {
                 if sd == Direction::N {
@@ -174,7 +174,6 @@ pub struct EventHandler {
     debug: bool,
 }
 
-// This looks almost too much like a 'god object'
 impl EventHandler {
     pub fn new(config: Rc<Config>, debug: bool) -> Self {
         Self {
@@ -221,26 +220,28 @@ impl EventHandler {
     pub fn main_loop(&mut self, input: &mut Libinput) {
         let fds = PollFd::new(input.as_raw_fd(), PollFlags::POLLIN);
         while poll(&mut [fds], -1).is_ok() {
-            self.handle_event(input);
+            self.handle_event(input)
+                .expect("An Error occurred while handling an event");
         }
     }
 
-    pub fn handle_event(&mut self, input: &mut Libinput) {
+    pub fn handle_event(&mut self, input: &mut Libinput) -> Result<()> {
         input.dispatch().unwrap();
         for event in input.clone() {
             if let Event::Gesture(e) = event {
                 match e {
-                    GestureEvent::Pinch(e) => self.handle_pinch_event(e),
-                    GestureEvent::Swipe(e) => self.handle_swipe_event(e),
-                    GestureEvent::Hold(e) => self.handle_hold_event(e),
+                    GestureEvent::Pinch(e) => self.handle_pinch_event(e)?,
+                    GestureEvent::Swipe(e) => self.handle_swipe_event(e)?,
+                    GestureEvent::Hold(e) => self.handle_hold_event(e)?,
                     _ => (),
                 }
             }
             input.dispatch().unwrap();
         }
+        Ok(())
     }
 
-    fn handle_hold_event(&mut self, event: GestureHoldEvent) {
+    fn handle_hold_event(&mut self, event: GestureHoldEvent) -> Result<()> {
         match event {
             GestureHoldEvent::Begin(e) => {
                 self.event = Gesture::Hold(Hold {
@@ -254,7 +255,7 @@ impl EventHandler {
                     for i in &self.config.clone().gestures {
                         if let Gesture::Hold(j) = i {
                             if j.fingers == s.fingers {
-                                exec_command_from_string(&j.action);
+                                exec_command_from_string(&j.action)?;
                             }
                         }
                     }
@@ -262,9 +263,10 @@ impl EventHandler {
             }
             _ => (),
         }
+        Ok(())
     }
 
-    fn handle_pinch_event(&mut self, event: GesturePinchEvent) {
+    fn handle_pinch_event(&mut self, event: GesturePinchEvent) -> Result<()> {
         match event {
             GesturePinchEvent::Begin(e) => {
                 self.event = Gesture::Pinch(Pinch {
@@ -287,7 +289,7 @@ impl EventHandler {
                                 && j.repeat == Repeat::Continuous
                             {
                                 if_debug!(self.debug, "continuous pinch gesture");
-                                exec_command_from_string(&j.action);
+                                exec_command_from_string(&j.action)?;
                             }
                         }
                     }
@@ -306,7 +308,7 @@ impl EventHandler {
                         if let Gesture::Pinch(j) = i {
                             if j.direction == s.direction && j.fingers == s.fingers {
                                 if_debug!(self.debug, "oneshot pinch gesture");
-                                exec_command_from_string(&j.action);
+                                exec_command_from_string(&j.action)?;
                             }
                         }
                     }
@@ -314,9 +316,10 @@ impl EventHandler {
             }
             _ => (),
         }
+        Ok(())
     }
 
-    fn handle_swipe_event(&mut self, event: GestureSwipeEvent) {
+    fn handle_swipe_event(&mut self, event: GestureSwipeEvent) -> Result<()> {
         match event {
             GestureSwipeEvent::Begin(e) => {
                 self.event = Gesture::Swipe(Swipe {
@@ -338,7 +341,7 @@ impl EventHandler {
                                 && j.direction == swipe_dir
                                 && j.repeat == Repeat::Continuous
                             {
-                                exec_command_from_string(&j.action);
+                                exec_command_from_string(&j.action)?;
                             }
                         }
                     }
@@ -356,7 +359,7 @@ impl EventHandler {
                         for i in &self.config.clone().gestures {
                             if let Gesture::Swipe(j) = i {
                                 if j.fingers == s.fingers && j.direction == s.direction {
-                                    exec_command_from_string(&j.action);
+                                    exec_command_from_string(&j.action)?;
                                 }
                             }
                         }
@@ -365,6 +368,7 @@ impl EventHandler {
             }
             _ => (),
         }
+        Ok(())
     }
 }
 
@@ -385,14 +389,4 @@ impl LibinputInterface for Interface {
             File::from_raw_fd(fd);
         }
     }
-}
-
-pub fn exec_command_from_string(s: &str) {
-    // let args = s.split(' ');
-    // dbg!(&args);
-    Command::new("sh")
-        .arg("-c")
-        .arg(s)
-        .spawn()
-        .expect("Could not execute external command");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,9 @@
+use anyhow::Result;
+use std::process::Command;
 
+pub fn exec_command_from_string(s: &str) -> Result<()> {
+    // let args = s.split(' ');
+    // dbg!(&args);
+    Command::new("sh").arg("-c").arg(s).spawn()?;
+    Ok(())
+}
