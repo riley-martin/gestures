e4a0aca967cd45eec72fdb35dbc8c189e875534d
J. F. Zhang <zhangjianfei3@gmail.com>
2023-02-21 08:18:26 +0800

    fix: Wait for the child process to exit (#9)

--- src/utils.rs
+++ src/utils.rs
@@ -3,17 +3,21 @@ use regex::Regex;
 use std::process::Command;
 
 pub fn exec_command_from_string(args: &str, dx: f64, dy: f64, scale: f64) -> Result<()> {
-    let rx = Regex::new(r"[^\\]\$delta_x").into_diagnostic()?;
-    let ry = Regex::new(r"[^\\]\$delta_y").into_diagnostic()?;
-    let rs = Regex::new(r"[^\\]\$scale").into_diagnostic()?;
-    let args = ry.replace_all(args, format!(" {dy} "));
-    let args = rx.replace_all(&args, format!(" {dx} "));
-    let args = rs.replace_all(&args, format!(" {scale} "));
-    log::debug!("{:?}", &args);
-    Command::new("sh")
-        .arg("-c")
-        .arg(&*args)
-        .spawn()
-        .into_diagnostic()?;
+    if !&args.is_empty() {
+        let rx = Regex::new(r"[^\\]\$delta_x").into_diagnostic()?;
+        let ry = Regex::new(r"[^\\]\$delta_y").into_diagnostic()?;
+        let rs = Regex::new(r"[^\\]\$scale").into_diagnostic()?;
+        let args = ry.replace_all(args, format!(" {dy} "));
+        let args = rx.replace_all(&args, format!(" {dx} "));
+        let args = rs.replace_all(&args, format!(" {scale} "));
+        log::debug!("{:?}", &args);
+        Command::new("sh")
+            .arg("-c")
+            .arg(&*args)
+            .spawn()
+            .into_diagnostic()?
+            .wait()
+            .into_diagnostic()?;
+    }
     Ok(())
 }
