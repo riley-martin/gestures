c52f20b8e5cad5d374a1606af3b1a646c3f4506f
Riley Martin <106552431+riley-martin@users.noreply.github.com>
2023-03-05 14:47:55 -0500

    Spawn children in a separate thread

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,26 @@
-use miette::{IntoDiagnostic, Result};
+use miette::Result;
 use regex::Regex;
 use std::process::Command;
 
 pub fn exec_command_from_string(args: &str, dx: f64, dy: f64, scale: f64) -> Result<()> {
     if !&args.is_empty() {
-        let rx = Regex::new(r"[^\\]\$delta_x").into_diagnostic()?;
-        let ry = Regex::new(r"[^\\]\$delta_y").into_diagnostic()?;
-        let rs = Regex::new(r"[^\\]\$scale").into_diagnostic()?;
-        let args = ry.replace_all(args, format!(" {dy} "));
-        let args = rx.replace_all(&args, format!(" {dx} "));
-        let args = rs.replace_all(&args, format!(" {scale} "));
-        log::debug!("{:?}", &args);
-        Command::new("sh")
-            .arg("-c")
-            .arg(&*args)
-            .spawn()
-            .into_diagnostic()?
-            .wait()
-            .into_diagnostic()?;
+        let args = args.to_string();
+        std::thread::spawn(move || {
+            let rx = Regex::new(r"[^\\]\$delta_x").unwrap();
+            let ry = Regex::new(r"[^\\]\$delta_y").unwrap();
+            let rs = Regex::new(r"[^\\]\$scale").unwrap();
+            let args = ry.replace_all(&args, format!(" {dy} "));
+            let args = rx.replace_all(&args, format!(" {dx} "));
+            let args = rs.replace_all(&args, format!(" {scale} "));
+            log::debug!("{:?}", &args);
+            Command::new("sh")
+                .arg("-c")
+                .arg(&*args)
+                .spawn()
+                .unwrap()
+                .wait()
+                .unwrap();
+        });
     }
     Ok(())
 }
