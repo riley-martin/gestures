fad77c4a2d0dbad214726b4d3cba7c66fb95c248
Riley Martin <106552431+riley-martin@users.noreply.github.com>
2023-03-12 12:45:07 -0400

    Support for rotation (#10)
    Add rotate support to pinch gestures.

--- src/utils.rs
+++ src/utils.rs
@@ -2,16 +2,18 @@ use miette::Result;
 use regex::Regex;
 use std::process::Command;
 
-pub fn exec_command_from_string(args: &str, dx: f64, dy: f64, scale: f64) -> Result<()> {
+pub fn exec_command_from_string(args: &str, dx: f64, dy: f64, da: f64, scale: f64) -> Result<()> {
     if !&args.is_empty() {
         let args = args.to_string();
         std::thread::spawn(move || {
             let rx = Regex::new(r"[^\\]\$delta_x").unwrap();
             let ry = Regex::new(r"[^\\]\$delta_y").unwrap();
             let rs = Regex::new(r"[^\\]\$scale").unwrap();
+            let ra = Regex::new(r"[^\\]\$delta_angle").unwrap();
             let args = ry.replace_all(&args, format!(" {dy} "));
             let args = rx.replace_all(&args, format!(" {dx} "));
             let args = rs.replace_all(&args, format!(" {scale} "));
+            let args = ra.replace_all(&args, format!(" {da} "));
             log::debug!("{:?}", &args);
             Command::new("sh")
                 .arg("-c")
